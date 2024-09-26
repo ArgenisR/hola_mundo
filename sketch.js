3259f7375db351a8541cb0e4f85bda2294c2e834
ArgenisR <argenis.ramos@ucuenca.edu.ec>
2024-09-26 16:27:40 -0500

    Se tuneo el cursor

--- sketch.js
+++ sketch.js
@@ -1,13 +1,26 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(144);
+  frameRate(40);
   background(0);
   rectMode(CENTER);
 }
 function draw() {
-  background(20, random(90, 60), 60);
-  fill(mouseY, 20, 200);
-  circle(mouseX + 20, mouseY - 50, mouseX);
-  fill(100, 50, 100);
-  rect(mouseX, mouseY, 20, 50);
+  background(20, random(90, 60), 60, 30);
+  stroke(0, 0, 0);
+  fill(mouseY, mouseX, 200);
+  stroke(20, 400, 20);
+  circle(mouseX, mouseY, 30, 30);
+  stroke(20, 100, 20);
+  rect(mouseX, mouseY, 20, 20);
+  rotate(mouseX);
+  stroke(0, 0, 0);
+  fill(mouseY, mouseX, 200);
+  circle(mouseX - 50, mouseY, 30, 30);
+  rotate(mouseX, mouseY);
+  stroke(20, 100, 20);
+  rect(mouseX - 50, mouseY, 20, 20);
+  stroke(300, 300, 300);
+  triangle(20, 20, 20, 20, mouseX, mouseY);
+  fill(mouseX, mouseY, 200);
+  rect(mouseX, 40, 30, 30);
 }
